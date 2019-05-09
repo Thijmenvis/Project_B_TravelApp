a878c13653233bb8a844dbc6a54093d40cce16cc
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-05-09 10:29:17 +0200

    Biografie aanpassen
    Biografie tekst kun je aanpassen/en slaat op op de pagina.

--- app/src/main/java/com/example/prototype/ProfileActivity.java
+++ app/src/main/java/com/example/prototype/ProfileActivity.java
@@ -1,16 +1,78 @@
 package com.example.prototype;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageURI;
+
+    TextView textView;
+    AlertDialog dialog;
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        textView = (TextView)findViewById(R.id.text_Bio_Text);
+        dialog = new AlertDialog.Builder(this).create();
+        editText = new EditText(this);
+
+        dialog.setTitle("Edit bio");
+        dialog.setView(editText);
+
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Save Changes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                textView.setText(editText.getText());
+
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(textView.getText());
+                dialog.show();
+
+            }
+        });
+
+        button = (Button)findViewById(R.id.btn_ChangePic);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+
+            }
+        });
+    }
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageURI = data.getData();
+        }
     }
 
     public void goToMap (View view){
