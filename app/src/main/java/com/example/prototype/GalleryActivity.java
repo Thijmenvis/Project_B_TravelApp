5b419229ecfab3688b041094f0618853f4038f0b
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-05-22 11:50:01 +0200

    Foto maken en weergave
    Je kunt nu een foto nemen in de app en het ook zien

--- app/src/main/java/com/example/prototype/GalleryActivity.java
+++ app/src/main/java/com/example/prototype/GalleryActivity.java
@@ -1,13 +1,18 @@
 package com.example.prototype;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class GalleryActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,8 @@ public class GalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gallery);
 
         Button button = (Button) findViewById(R.id.button_2);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,7 +29,15 @@ public class GalleryActivity extends AppCompatActivity {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, 0);
 
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
--- app/src/main/java/com/example/prototype/SignUpActivity.java
+++ app/src/main/java/com/example/prototype/SignUpActivity.java
@@ -4,14 +4,54 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText Uname, Email, Password;
+    private Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setupUIViews();
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate()){
+
+                }
+            }
+        });
     }
+
+    private void setupUIViews(){
+        Uname = (EditText)findViewById(R.id.editText_Name);
+        Password = (EditText)findViewById(R.id.editText_Password);
+        Email = (EditText)findViewById(R.id.editText_Email);
+        SignUp = (Button)findViewById(R.id.button_SignUp);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+
+        String name = Uname.getText().toString();
+        String password = Password.getText().toString();
+        String email = Email.getText().toString();
+
+        if(name.isEmpty() && password.isEmpty() && email.isEmpty()) {
+            Toast.makeText(this, "Please fill in", Toast.LENGTH_SHORT).show();
+        }else{
+            result = true;
+        }
+
+        return result;
+    }
+
     public void goToMain (View view){
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
