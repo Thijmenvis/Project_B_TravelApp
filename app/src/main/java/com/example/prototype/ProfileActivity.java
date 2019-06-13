3ad2db29f33ccb7f7b1eeabb9fcf0f8979990f0a
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-06-14 00:50:28 +0200

    Take picture button back and log out

--- app/src/main/java/com/example/prototype/ProfileActivity.java
+++ app/src/main/java/com/example/prototype/ProfileActivity.java
@@ -2,17 +2,15 @@ package com.example.prototype;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Camera;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -75,6 +73,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+
         textView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,7 +81,9 @@ public class ProfileActivity extends AppCompatActivity {
                 dialog2.show();
 
             }
+
         });
+
 
         imageView = (ImageView)findViewById(R.id.image_Avatar);
         button = (Button)findViewById(R.id.btn_ChangePic);
@@ -94,11 +95,35 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+
+        ImageButton btn = (ImageButton)findViewById(R.id.imageButton2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton button = (ImageButton)findViewById(R.id.imageButton5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
     }
+
     private void openGallery(){
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
+
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -113,16 +138,6 @@ public class ProfileActivity extends AppCompatActivity {
 
     public void goToMap (View view){
         Intent intent = new Intent (this, MapActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToGallery (View view){
-        Intent intent = new Intent (this, GalleryActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToSettings (View view){
-        Intent intent = new Intent (this, SettingsActivity.class);
         startActivity(intent);
     }
 
